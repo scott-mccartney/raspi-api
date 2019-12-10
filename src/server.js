9ebe979ad9e99031cbd1f28a80148aa2ad36fd4d
smccartney09 <smccartney09@gmail.com>
2019-12-10 04:28:00 +0000

    Turns on pi camera

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const app = require('express')();
 var socket = require('socket.io');
+const PiCamera = require('pi-camera');
 
 const PORT_NO = 9876;
 const HEALTH_STATUS = { healthy: true };
@@ -17,9 +18,47 @@ app.get('/health', function (req, res) {
 var server = app.listen(PORT_NO, () => console.log(`Listening on Port ${PORT_NO}`));
 
 let io = socket(server)
-io.on('connection', (socket) => {
+io.on('connection', onConnect);
+
+function onConnect (socket) {
   console.log(`${socket.id} is connected`);
   socket.on('disconnect', (reason) => {
     console.log(`${socket.id} disconnected`)
   });
+
+  socket.on('timecheck', (data) => {
+    console.log(data);
+  });
+}
+
+const myCamera = new PiCamera({
+  mode: 'photo',
+  output: `${ __dirname }/imgs/test.jpg`,
+  width: 640,
+  height: 480,
+  nopreview: true
 });
+ 
+myCamera.snap()
+  .then((result) => {
+    console.log('Success');
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log('Error');
+    console.log(error);
+  });
+// var index = 0;
+// let myVar = setInterval(function(){ timer() }, 1000);
+
+// function timer() {
+//   if (index > 10) {
+//     stopFunction();
+//   }
+//   io.emit('timecheck', { time: '123'}); 
+//   index++;
+// }
+
+// function stopFunction() {
+//     clearInterval(myVar);
+// }
